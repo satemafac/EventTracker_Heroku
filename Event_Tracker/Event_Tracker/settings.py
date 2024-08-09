fc1b3ee460f6aeaa672847faa8d84615168c81cc
Stalanic <anustalanic@gmail.com>
2024-08-08 23:14:11 -0400

    settings update

--- Event_Tracker/Event_Tracker/settings.py
+++ Event_Tracker/Event_Tracker/settings.py
@@ -146,11 +146,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / "event/static",
+#     "/var/www/static/",
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 STATICFILES_DIRS = [
-    BASE_DIR / "event/static",
-    "/var/www/static/",
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, "static"),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media') #Media Upload folder
