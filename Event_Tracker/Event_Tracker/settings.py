d1408f9bedc0798bfe470269f56239330a98ac1a
Stalanic <anustalanic@gmail.com>
2024-08-12 23:12:07 -0400

    staticc

--- Event_Tracker/Event_Tracker/settings.py
+++ Event_Tracker/Event_Tracker/settings.py
@@ -149,7 +149,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, ''),
+    os.path.join(BASE_DIR, 'event/static'),
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR,'media') #Media Upload folder
 
