e81f5bc6e5fc8718be62dccac0cf1d48056c8cbf
Stalanic <anustalanic@gmail.com>
2024-08-12 22:42:46 -0400

    fixing static

--- Event_Tracker/Event_Tracker/settings.py
+++ Event_Tracker/Event_Tracker/settings.py
@@ -149,7 +149,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'Event_Tracker/event/static'),
+    os.path.join(BASE_DIR, 'event/static'),
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR,'media') #Media Upload folder
 
