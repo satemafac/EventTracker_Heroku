54bf8573a08aeb0c9518e57466e089742e1404a5
Stalanic <anustalanic@gmail.com>
2024-08-12 23:40:18 -0400

    settings update

--- Event_Tracker/Event_Tracker/settings.py
+++ Event_Tracker/Event_Tracker/settings.py
@@ -146,13 +146,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'event/static'),
+    BASE_DIR / "static",
+    "/var/www/static/",
+    os.path.join(BASE_DIR, 'static'),
 ]
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media') #Media Upload folder
+
+MEDIA_URL = '/' # can make this whatever url to generate files urls
 
 
 # Default primary key field type
