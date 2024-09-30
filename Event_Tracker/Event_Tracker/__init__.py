9cc844732a70e74be3acf17c7179627f33a7ce2c
Stalanic <anustalanic@gmail.com>
2024-09-30 18:35:48 -0400

    initt1

--- Event_Tracker/Event_Tracker/__init__.py
+++ Event_Tracker/Event_Tracker/__init__.py
@@ -1,3 +1,5 @@
-from .celery import app as celery_app
+import os
 
-__all__ = ('celery_app',)
+if os.environ.get('CELERY_WORKER_RUNNING') == '1':
+    from .celery import app as celery_app
+    __all__ = ('celery_app',)
